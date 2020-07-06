7c903e9849af5dc917ceb6cf182d15f2103a2a0f
Gianna Gandossi <Gianna.Gandossi@hutton.ac.uk>
2020-07-06 14:46:06 +0100

    from timeslider to dumbslider

--- js/map-config.js
+++ js/map-config.js
@@ -124,7 +124,7 @@ require([
         ]
     });
 
-    const layerRenderer = new RasterStretchRenderer({
+    const countOfDayRenderer = new RasterStretchRenderer({
         colorRamp: combineColorRamp,
         stretchType: 'min-max'
             //statistics: [
@@ -165,10 +165,10 @@ require([
         // title: [],
         url: 'https://druid.hutton.ac.uk/arcgis/rest/services/Agromet_Indicators/plantheatstress_test/ImageServer',
         mosaicRule: mosaicRule,
-        renderer: layerRenderer,
+        renderer: countOfDayRenderer,
         opacity: 0.9,
         popupTemplate: {
-            title: '{Raster.ServicePixelValue} plant heat stress day(s) in {Year}'
+            title: '{Raster.ServicePixelValue} plant heat stress day(s) in {Year}' // need to customize based on variable
                 //content: [] //'{Raster.ServicePixelValue}'
         },
     });
@@ -219,56 +219,137 @@ require([
     };
 
     //listen to change events on ensembleSelect
-    const ensembleSelect = document.getElementById('ensembleSelect');
-    ensembleSelect.addEventListener('change', function() {
-        const chosenEnsemble = ensembleSelect.value;
-        changeEnsemble(chosenEnsemble);
-    });
+    // const ensembleSelect = document.getElementById('ensembleSelect');
+    // ensembleSelect.addEventListener('change', function() {
+    //     const chosenEnsemble = ensembleSelect.value;
+    //     changeEnsemble(chosenEnsemble);
+    // });
 
-    function changeEnsemble(chosenEnsemble) {
+    // function changeEnsemble(chosenEnsemble) {
 
-        const mosaicRuleClone = plantHeatStressLayer.mosaicRule.clone(); // makes clone of layer's mosaicRule
-        const ensembleVariable = mosaicRuleClone.multidimensionalDefinition;
-        switch (chosenEnsemble) {
-            case 'ensemble 1':
-                ensembleVariable.variableName = 'UKCP18_12KM_UK_corrected1km_ens01';
-                mosaicRuleClone.multidimensionalDefinition = ensembleVariable;
-                plantHeatStressLayer.mosaicRule = mosaicRuleClone;
-                break;
-            case 'ensemble 4':
-                ensembleVariable.variableName = 'UKCP18_12KM_UK_corrected1km_ens04';
-                mosaicRuleClone.multidimensionalDefinition = ensembleVariable;
-                plantHeatStressLayer.mosaicRule = mosaicRuleClone;
-                break;
-            case 'ensemble 5':
-                ensembleVariable.variableName = 'UKCP18_12KM_UK_corrected1km_ens05';
-                mosaicRuleClone.multidimensionalDefinition = ensembleVariable;
-                plantHeatStressLayer.mosaicRule = mosaicRuleClone;
-                break;
-        }
+    //     const mosaicRuleClone = plantHeatStressLayer.mosaicRule.clone(); // makes clone of layer's mosaicRule
+    //     const ensembleVariable = mosaicRuleClone.multidimensionalDefinition;
+    //     switch (chosenEnsemble) {
+    //         case 'ensemble 1':
+    //             ensembleVariable.variableName = 'UKCP18_12KM_UK_corrected1km_ens01';
+    //             mosaicRuleClone.multidimensionalDefinition = ensembleVariable;
+    //             plantHeatStressLayer.mosaicRule = mosaicRuleClone;
+    //             break;
+    //         case 'ensemble 4':
+    //             ensembleVariable.variableName = 'UKCP18_12KM_UK_corrected1km_ens04';
+    //             mosaicRuleClone.multidimensionalDefinition = ensembleVariable;
+    //             plantHeatStressLayer.mosaicRule = mosaicRuleClone;
+    //             break;
+    //         case 'ensemble 5':
+    //             ensembleVariable.variableName = 'UKCP18_12KM_UK_corrected1km_ens05';
+    //             mosaicRuleClone.multidimensionalDefinition = ensembleVariable;
+    //             plantHeatStressLayer.mosaicRule = mosaicRuleClone;
+    //             break;
+    //     }
 
-    };
+    // };
 
     /************************************
-     * TimeSlider
+     * Slider
      *************************************/
-    const timeSlider = new TimeSlider({
-        container: 'timeSlider',
-        mode: 'instant',
-        view: view,
-        playRate: 300, // default is 1000 milliseconds
-        fullTimeExtent: {
-            start: new Date(1961, 0, 1),
-            end: new Date(2080, 0, 1)
+    const yearSlider = new Slider({
+        container: 'yearSlider',
+        min: 1961,
+        max: 2080,
+        values: [1961],
+        precision: 0,
+        snapOnClickEnabled: true,
+        visibleElements: {
+            labels: true,
+            rangeLabels: true
         },
-        stops: {
-            interval: {
-                value: 1,
-                unit: 'years'
-            }
+        steps: 119,
+        tickConfigs: [{
+            mode: 'count',
+            values: 119,
+            labelsVisible: false,
+        }]
+    });
+
+    // when the user changes the yearSlider's value, change the year to reflect data
+    yearSlider.on(['thumb-change', 'thumb-drag'], function(event) {
+        stopAnimation();
+        updateYearDef(event.value);
+    });
+
+    function updateYearDef(value) {
+        const mosaicRuleClone = plantHeatStressLayer.mosaicRule.clone(); // makes clone of layer's mosaicRule
+        const yearVariable = mosaicRuleClone.multidimensionalDefinition;
+        yearVariable.values = yearSlider.get('values');
+        mosaicRuleClone.multidimensionalDefinition = yearVariable;
+        plantHeatStressLayer.mosaicRule = mosaicRuleClone;
+    };
+
+    // set vars for play button 
+    const playButton = document.getElementById('playButton');
+    let animation = null;
+
+    // When user drags the slider:
+    //  - stops the animation
+    //  - set the visualized year to the slider.
+    function inputHandler(event) {
+        stopAnimation();
+        updateYearDef(event.value);
+    };
+
+    // Toggle animation on/off when user
+    // clicks on the play button
+    playButton.addEventListener('click', function() {
+        if (playButton.classList.contains('toggled')) {
+            stopAnimation();
+        } else {
+            startAnimation();
         }
     });
-    view.ui.add(timeSlider, 'bottom');
+    let timerId = 0;
+
+    // Starts the animation that cycle through the years
+    function startAnimation() {
+        stopAnimation();
+        timerId = setInterval(() => {
+            let year = yearSlider.values[0];
+            year += 1;
+            if (year > yearSlider.max) {
+                year = yearSlider.min;
+            }
+            yearSlider.values = [year];
+            updateYearDef(year);
+        }, 1000)
+        playButton.classList.add('toggled');
+    };
+
+    // Stops the animation
+    function stopAnimation() {
+        if (!timerId) {
+            return;
+        }
+        clearTimeout(timerId);
+        timerId = 0;
+        playButton.classList.remove('toggled');
+    };
+
+    // const timeSlider = new TimeSlider({
+    //     container: 'timeSlider',
+    //     mode: 'instant',
+    //     view: view,
+    //     playRate: 300, // default is 1000 milliseconds
+    //     fullTimeExtent: {
+    //         start: new Date(1961, 0, 1),
+    //         end: new Date(2080, 0, 1)
+    //     },
+    //     stops: {
+    //         interval: {
+    //             value: 1,
+    //             unit: 'years'
+    //         }
+    //     }
+    // });
+    // view.ui.add(timeSlider, 'bottom');
 
 
 
