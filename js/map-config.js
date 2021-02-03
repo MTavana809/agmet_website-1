54dca893ae17d93b77ddbf49e6c4643c97d482fd
Gianna Gandossi <Gianna.Gandossi@hutton.ac.uk>
2021-02-03 12:22:31 +0000

    experiments with histograms

--- js/map-config.js
+++ js/map-config.js
@@ -1,20 +1,5 @@
 'use strict'; // https://javascript.info/strict-mode for explanation
 
-function toggleDescription() {
-    const hideaway = document.getElementById("hideaway");
-    const frame = document.getElementById("frame");
-    const button = document.getElementById("button");
-
-    if (hideaway.style.display === "none") {
-        hideaway.style.display = "block";
-        button.innerHTML = "&#x25BC; Hide Info";
-        frame.style.width = "70%";
-    } else {
-        hideaway.style.display = "none";
-        frame.style.width = "100%";
-        button.innerHTML = "&#x25B2; Show Info";
-    }
-};
 
 require([
     'esri/Map',
@@ -507,6 +492,7 @@ require([
         timerId = 0;
         playButton.classList.remove('toggled');
     };
+
 
     /******************************
      * Small ui widgets
